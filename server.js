cd3ea8becf1f78602e6ed16ce06956fe257237d8
gwenutley <gwen.utley24@gmail.com>
2025-11-12 12:05:39 -0700

    week3 learn activities

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const baseController = require("./controllers/baseController")
+const inventoryRoute = require("./routes/inventoryRoute")
 
 //use public files statically
 app.use(express.static("public"));
@@ -27,10 +29,9 @@ app.set("layout", "./layouts/layout") //not
  *************************/
 //app.use(require("./routes/static"))
 //Index route
-
-app.get("/", function (req, res) {
-  res.render("index", {title: "Home"})
-})
+app.get("/", baseController.buildHome)
+// Inventory routes
+app.use("/inv", inventoryRoute)
 
 /* ***********************
  * Local Server Information
